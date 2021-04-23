94c24245d51fe0335f2871feebdfd1127b784bf5
iotsharing.com <nha.tuan84@gmail.com>
2021-04-23 13:22:34 +0700

    Update setup_cython.py

--- setup_cython.py
+++ setup_cython.py
@@ -4,8 +4,9 @@ import sysconfig
 
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py as _build_py
-
+from distutils.extension import Extension
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 EXCLUDE_FILES = [
 ]
@@ -14,30 +15,25 @@ EXCLUDE_FILES = [
 def get_ext_paths(root_dir, exclude_files):
     """get filepaths for compilation"""
     paths = []
-
     for root, dirs, files in os.walk(root_dir):
         for filename in files:
             if os.path.splitext(filename)[1] != '.py':
                 continue
-
             file_path = os.path.join(root, filename)
             if file_path in exclude_files:
                 continue
-
             paths.append(file_path)
     return paths
 
 class build_py(_build_py):
     def find_package_modules(self, package, package_dir):
-        ext_suffix = '.py'
         modules = super().find_package_modules(package, package_dir)
         filtered_modules = []
         for (pkg, mod, filepath) in modules:
-            if os.path.exists(filepath.replace('.py', ext_suffix)):
+            if filepath.endswith('.py'):
                 continue
             filtered_modules.append((pkg, mod, filepath, ))
         return filtered_modules
-
 
 setup(
     name='app',
@@ -45,12 +41,13 @@ setup(
     packages=find_packages(),
     ext_modules=cythonize(
         get_ext_paths('app', EXCLUDE_FILES),
+        exclude=["app/main.py"],
         compiler_directives={'language_level': 3}
     ),
     cmdclass={
         'build_py': build_py
     },
     package_data={
-        "app": ["models/*", "main.py"],#"main.py" is running file
+        "app": ['data/*', "main.py"],
     }
 )
